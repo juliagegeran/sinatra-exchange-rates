886acb7903089c014fd05af654de20e83c03f3de
Julia Ran <59769618+juliagegeran@users.noreply.github.com>
2023-10-26 01:30:13 +0000

    home

--- app.rb
+++ app.rb
@@ -1,8 +1,31 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "json"
 
 get("/") do
-  api_url = "https://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
+  exchange_rate_key = ENV["EXCHANGE_RATE_API"]
+  
+  api_url = "https://api.exchangerate.host/list?access_key=#{exchange_rate_key}"
+  
+  #place a GET request to the URL
+  raw_data = HTTP.get(api_url)
+
+  #convert raw requests to a string
+  raw_data_string = raw_data.to_s
+
+  #parse JSON response
+  parsed_data = JSON.parse(raw_data) 
+  
+  #get the symbols from JSON
+  @currencies = parsed_data.fetch("currencies")
+
+  #get symbols
+  @symbols = @currencies.keys
+
+  #render a view template where I show the symbols
   
   erb(:home)
+
+  
 end
