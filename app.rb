4a6996ba533898f538c9dce14b69512e1e2869cb
Julia Ran <59769618+juliagegeran@users.noreply.github.com>
2023-10-26 16:59:10 +0000

    done

--- app.rb
+++ app.rb
@@ -21,11 +21,49 @@ get("/") do
   @currencies = parsed_data.fetch("currencies")
 
   #get symbols
-  @symbols = @currencies.keys
+  @from_currency = @currencies.keys
 
-  #render a view template where I show the symbols
-  
   erb(:home)
-
   
+end
+
+get("/:from_currency") do
+  exchange_rate_key = ENV["EXCHANGE_RATE_API"]
+  api_url = "https://api.exchangerate.host/list?access_key=#{exchange_rate_key}"  
+  @original_currency = params.fetch("from_currency")
+
+  #place a GET request to the URL
+  raw_data = HTTP.get(api_url)
+
+  #convert raw requests to a string
+  raw_data_string = raw_data.to_s
+
+  #parse JSON response
+  parsed_data = JSON.parse(raw_data) 
+  
+  #get the symbols from JSON
+  @currencies = parsed_data.fetch("currencies")
+
+  #get symbols
+  @to_currencies = @currencies.keys
+
+  erb(:from_currency)
+end
+
+
+get("/:from_currency/:to_currency") do
+  @original_currency = params.fetch("from_currency")
+  @destination_currency = params.fetch("to_currency")
+  exchange_rate_key = ENV["EXCHANGE_RATE_API"]
+  api_url = "https://api.exchangerate.host/convert?access_key=#{exchange_rate_key}&from=#{@original_currency}&to=#{@destination_currency}&amount=1"
+
+  #get raw data
+  raw_data = HTTP.get(api_url).to_s
+  #get parsed data
+  parsed_data = JSON.parse(raw_data)
+
+  #get results
+  @conversion = parsed_data.fetch("result").to_s
+
+  erb(:conversion)
 end
